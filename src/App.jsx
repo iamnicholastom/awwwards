e78535c1d60658a35c3b34cb2b4d0a7f9959971b
Nicholas Tom <ntompg@gmail.com>
2024-12-27 22:50:57 +0530

    add tailwind config

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 const App = () => {
-  return <div>App</div>;
+  return (
+    <main>
+      <h1 className="text-5xl text-orange-500 font-bold text-violet-300">
+        Welcome to Awwwards
+      </h1>
+    </main>
+  );
 };
 
 export default App;
